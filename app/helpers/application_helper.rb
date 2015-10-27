1b9489ab1471919199448f7785436c40a1bbe9e2
khcr <kocher.ke@gmail.com>
2015-10-27 15:02:13 +0100

    small fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
   def options_for_enum(record, enum)
     model = record.class
     pluralized_enum = enum.to_s.pluralize
-    options_for_select(model.send(pluralized_enum).map {|k, v| [ t("#{model.to_s.downcase}.#{pluralized_enum}.#{k}"), k ]}, record.send(enum))
+    options_for_select(model.send(pluralized_enum).map {|k, v| [ t("#{model.model_name.element}.#{pluralized_enum}.#{k}"), k ]}, record.send(enum))
   end
 
 end
