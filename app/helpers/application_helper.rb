0d61d4760604f1331175bfcd7b0833ad2bac8117
Noémien Kocher <nkcr.je@gmail.com>
2016-02-02 13:41:52 +0100

    menu design

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -6,4 +6,20 @@ module ApplicationHelper
     options_for_select(model.send(pluralized_enum).map {|k, v| [ t("#{model.model_name.i18n_key}.#{pluralized_enum}.#{k}"), k ]}, record.send(enum))
   end
 
+  def active_link_to(text, link, route, options = {})
+    controller, action = route.split("#")
+    if controller == request.params[:controller]
+      options[:class] = "active" if request.params[:action] == action || action == nil
+    end
+    link_to(text, link, options)
+  end
+
+  def active_block(route, options = {}, &block)
+    controller, action = route.split("#")
+    if controller == request.params[:controller]
+      options[:class] = "active" if request.params[:action] == action || action == nil
+    end
+    content_tag(:div, options, &block)
+  end
+
 end
