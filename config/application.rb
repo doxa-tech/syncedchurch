1884e9e82f70acaecbecf1e186719982f4a09d79
khcr <kocher.ke@gmail.com>
2016-05-14 11:32:05 +0200

    force SSL

--- config/application.rb
+++ config/application.rb
@@ -24,7 +24,7 @@ module Syncedchurch
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    
+
     config.i18n.load_path += Dir[Rails.root.join('config', 'locales', '**', '*.{rb,yml}')]
 
     # generators
@@ -37,5 +37,7 @@ module Syncedchurch
       g.helper          false
       g.factory_girl    false
     end
+
+    config.force_ssl = true
   end
 end
